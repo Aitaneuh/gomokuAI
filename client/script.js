3c9f857d2a66785df94a789aa59a4b891254c0e7
Aitaneuh <ethanb9407@gmail.com>
2026-02-25 10:06:09 +0100

    moved object creation inside fonction to avoid errors

--- client/script.js
+++ client/script.js
@@ -3,13 +3,12 @@ import PositionHelper from "./position_helper.js";
 import GameEngine from "./gameEngine.js";
 import PlayerSelectHelper from "./player_select_helper.js";
 
-// Creating elements
-const renderer = new Renderer()
-const positionHelper = new PositionHelper()
-const gameEngine = new GameEngine()
-const playerSelectHelper = new PlayerSelectHelper()
-
 window.addEventListener("load", (event) => {
+    const renderer = new Renderer()
+    const positionHelper = new PositionHelper()
+    const gameEngine = new GameEngine()
+    const playerSelectHelper = new PlayerSelectHelper()
+    
     let position = positionHelper.getUrlPosition()
     renderer.drawBoard(position)
     renderer.handleAnalysisTabs()
